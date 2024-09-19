81b3c38ea51ccba45260f0668812dd2021715a57
vojtik <148624206+Vojtik1112@users.noreply.github.com>
2024-09-19 12:51:10 +0200

    yxc

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const wallpaperButton = document.querySelector('.change-wallpaper');
 
 wallpaperButton.addEventListener('click', function() {
-  document.body.style.backgroundImage = "url('new-wallpaper.jpg')"; // Replace with the new wallpaper path
+  document.body.style.backgroundImage = "url('images/new-wallpaper.jpg')"; // Local file path
 });
