0efe1b196c745b1fe69ec7dd9c63b0dad2c1ae7c
vojtik <vojtech1novak@seznam.cz>
2024-09-21 21:36:20 +0200

    pre3d

--- MainPage/script.js
+++ MainPage/script.js
@@ -3,3 +3,13 @@ window.onload = function() {
         document.getElementById('overlay').style.display = 'none';
     }, 3000); // 3000 milliseconds = 3 seconds
 };
+
+// JavaScript to delay audio playback
+window.addEventListener('load', function() {
+    const audio = document.getElementById('myAudio');
+    const delay = 3000; // Delay in milliseconds (3000ms = 3 seconds)
+
+    setTimeout(() => {
+        audio.play();
+    }, delay);
+});
