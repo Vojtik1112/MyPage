303c14f010cb228f215e1036fc71f80e090a2d82
vojtik <vojtech1novak@seznam.cz>
2024-09-22 21:20:08 +0200

    x

--- MainPage/script.js
+++ MainPage/script.js
@@ -1,13 +1,13 @@
 window.onload = function() {
     setTimeout(function() {
         document.getElementById('overlay').style.display = 'none';
-    }, 3000); // 3000 milliseconds = 3 seconds
+    }, 2000); // 2000 milliseconds = 2 seconds
 };
 
 // JavaScript to delay audio playback
 window.addEventListener('load', function() {
     const audio = document.getElementById('myAudio');
-    const delay = 3000; // Delay in milliseconds (3000ms = 3 seconds)
+    const delay = 2000; // Delay in milliseconds (2000ms = 2 seconds)
 
     setTimeout(() => {
         audio.play();
